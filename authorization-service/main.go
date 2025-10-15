508940e2daa679ea9f1798474fc075935a77b111
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-10-15 16:27:28 +0200

    load database url

--- authorization-service/main.go
+++ authorization-service/main.go
@@ -1,11 +1,20 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 
 	err := godotenv.Load()
 	if err != nil {
 		panic("Error loading .env file")
+	}
+
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl == "" {
+		panic("DATABASE_URL not set!")
 	}
 }
