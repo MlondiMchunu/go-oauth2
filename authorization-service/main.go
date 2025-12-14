ae290ad471b093b1071346aa1f3878cee71f5784
MlondiMchunu <mlondiemchunu1@gmail.com>
2025-12-14 22:46:50 +0200

    check for client

--- authorization-service/main.go
+++ authorization-service/main.go
@@ -100,6 +100,12 @@ func main() {
 			return c.Status(400).JSON(fiber.Map{"error": "invalid_request"})
 		}
 
+		//Check for client
+		client := new(Client)
+		if err := db.Where("id = ?", authRequest.ClientID).First(&client).Error; err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "invalid_request"})
+		}
+
 		return c.SendString("auth!")
 	})
 
